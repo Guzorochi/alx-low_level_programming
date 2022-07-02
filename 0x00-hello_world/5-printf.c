58662a65f102b2b02083508a35bbe26db30bbd85
Confidence <guzorochinwankwo@gmail.com>
2022-07-02 14:05:15 +0000

    Preprocessor

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * Description: main - Betty things
+ * Nice getting to this point
+ * Return: successful
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+	return (0);
+}
