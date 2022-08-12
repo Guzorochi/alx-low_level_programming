7bcfa6ba20f7f16ea8039b1e449f05d2c35674cd
Confidence <guzorochinwankwo@gmail.com>
2022-08-12 20:41:45 +0000

    A function that adds a list in a SLL

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - this prints all the elements in a linked list
+ * @h: pointer to the list_t list to be displayed
+ *
+ * Return:  Return the number of nodes displayed
+ */
+
+size_t print_list(const list_t *h);
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			nodes++;
+			continue;
+		}
+		
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nodes++;
+	}
+	return (nodes);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,1 @@
+This project is on singly list. Presenting data in a linear format like in queue of arrays. Singly lists has other data types such as Circular lists and doubly list.
